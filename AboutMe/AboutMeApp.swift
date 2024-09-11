ac4eb183aa3540d5cfa2cb52c77c2040b5426be9
szholbarys <sakenzholbarys@gmail.com>
2024-09-11 23:26:45 +0500

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Zholbarys on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Zholbarys on 11.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
